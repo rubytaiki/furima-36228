2f464d89c96a042d6f947d3417158eb40593f6fc
rubytaiki <taikimalay@gmail.com>
2021-07-23 21:21:23 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,24 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :description
+    validates :price, numericality:{in:300..9999999}, format:{ with: /\A[0-9]+\z/ }
+  end
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :status_id
+    validates :prefecture_id
+    validates :charge_id
+    validates :shipping_date_id
+  end
+
 end
