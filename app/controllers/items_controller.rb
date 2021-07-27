953f4694df94c4a416b0baedc2f79cb7b5c77ab9
rubytaiki <taikimalay@gmail.com>
2021-07-27 17:40:09 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :find_params, only: [:edit, :update, :move_to_index]
   before_action :move_to_index, only: [:edit, :update]
 
