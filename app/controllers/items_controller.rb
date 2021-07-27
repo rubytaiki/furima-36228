e727e1c8a7b5ce0de29324425ecdd5ff5f495653
rubytaiki <taikimalay@gmail.com>
2021-07-27 17:08:08 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_params, only: [:edit, :update, :move_to_index]
-  before_action :move_to_index, only: [:edit, :update]
+  
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -40,10 +40,6 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :status_id, :charge_id, :prefecture_id,
                                  :shipping_date_id, :price, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
   end
 
   def find_params
